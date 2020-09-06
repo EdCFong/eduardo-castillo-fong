543c8f4911cc174c0b7fa3907e0fa272df6e15e2
Eduardo Castillo Fong <EDUARDCF90@GMAIL.COM>
2020-09-06 17:46:08 -0400

    Update September 6, 2020

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import MyPhoto from './imagen/Myself.png' ;
+import MyPhoto from './imagen/Myself.png';
 
 function App() {
   return (
@@ -26,30 +26,37 @@ function AboutMe() {
   return (
     <div id="Me">
       <div id="WhoIam">
-        <div id="box_who_i_am_Header">
-          <img id="My_photo" src={MyPhoto} alt="This is me" />
-          <h2 class="h2_left_3">Who I am?</h2>
+
+        <div id="WhoIam_Header">
+          <img src={MyPhoto} alt="This is me" />
+          <h2>Who I am?</h2>
         </div>
         <p> I am a student of computerized information systems at the <a href="http://ut.suagm.edu"
-            target="_blank">University of Turabo</a> and I reside inFlorida, USA. Expecting graduate in 2021,
-             I work for be part of the developer community, learning every day new cool technologies.</p>
+          target="_blank" rel="noopener noreferrer">University of Turabo</a> and I reside inFlorida,
+           USA. Expecting graduate in 2021, I work for be part of the developer community, learning
+           every day new cool technologies.</p>
         <p>Do you want to see what I'm doing? Let's take a look below.</p>
-      <div>
-        <a id="arrow" href="#achievements"><span class="glyphicon glyphicon-arrow-right"></span></a>
+        <div id="arrowDiv">
+          <a id="arrow" href="#achievements"><i class="fas fa-arrow-right"></i></a>
+        </div>
+
       </div>
-    </div>
-    <div id="Skills">
-      <h2 class="h2_left">Technologies I use</h2>
-      <div id="technologies_list">
-        <p class="a_technology"><span class="glyphicon glyphicon-check technology_icon"></span>Universal
-                            Windows
-                            Platform (UWP)</p>
-        <p class="a_technology"><span class="glyphicon glyphicon-check technology_icon"></span>SQLite
-                        </p>
-        <p class="a_technology"><span class="glyphicon glyphicon-check technology_icon"></span>Bootstrap
-                        </p>
+
+
+
+      <div id="Skills">
+        <h2>My skills</h2>
+        <div id="skillsList">
+          
+             <div id="exampleSkill1">
+             <img class="skill_icon"
+                                src="https://lh3.googleusercontent.com/cx3ZlX60fw5H5SqcEbyuHtXr05jxMtx23JVL5RY8V61rkwfqWod01Ar0XJdW1u2qIXppbv_nXUXExUoP6b-b340dlIKcHTX9vt-DJCAetOuZ8tXqXWndEZcWS4uuaMyzFJZgj8FxfIw=w2400"
+                                alt="C# icon"/>
+                 <p>WPF</p>
+             </div>
+
+        </div>
       </div>
-    </div>
     </div >
 
   )
