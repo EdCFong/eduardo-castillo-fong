2bdeaf72a922b55d9a71d1556222046f15369365
Eduardo Castillo Fong <EDUARDCF90@GMAIL.COM>
2020-12-31 20:24:34 -0500

    Update December 31, 2020
    -Change color order
    -Adding new components

--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ function App() {
     <body>
       <Head />
       <AboutMe />
+      <Skills />
+      <Projects />
+      <Information />
     </body>
   );
 }
 
 function Head() {
   return (
-    <div id="Head">
-      <div id="welcome-section">
-        <h1>Hi, I'am Eduardo Castillo Fong</h1>
-        <h3>a web and software developer</h3>
+    <div id="head">
+      <div id="presentationMessage">
+        <h1>Hi, I'm Eduardo Castillo Fong</h1>
+        <h3><strong>&gt;_</strong> software developer and computer enthusiast</h3>
       </div>
     </div>
   )
@@ -25,41 +28,72 @@ function Head() {
 function AboutMe() {
   return (
     <div id="Me">
-      <div id="WhoIam">
-
-        <div id="WhoIam_Header">
-          <img src={MyPhoto} alt="This is me" />
-          <h2>Who I am?</h2>
-        </div>
-        <p> I am a student of computerized information systems at the <a href="http://ut.suagm.edu"
-          target="_blank" rel="noopener noreferrer">University of Turabo</a> and I reside inFlorida,
-           USA. Expecting graduate in 2021, I work for be part of the developer community, learning
-           every day new cool technologies.</p>
-        <p>Do you want to see what I'm doing? Let's take a look below.</p>
-        <div id="arrowDiv">
-          <a id="arrow" href="#achievements"><i class="fas fa-arrow-right"></i></a>
-        </div>
-
-      </div>
-
-
-
-      <div id="Skills">
-        <h2>My skills</h2>
-        <div id="skillsList">
-          
-             <div id="exampleSkill1">
-             <img class="skill_icon"
-                                src="https://lh3.googleusercontent.com/cx3ZlX60fw5H5SqcEbyuHtXr05jxMtx23JVL5RY8V61rkwfqWod01Ar0XJdW1u2qIXppbv_nXUXExUoP6b-b340dlIKcHTX9vt-DJCAetOuZ8tXqXWndEZcWS4uuaMyzFJZgj8FxfIw=w2400"
-                                alt="C# icon"/>
-                 <p>WPF</p>
-             </div>
-
-        </div>
-      </div>
-    </div >
-
+      <WhoIam />
+      <Certifications />
+    </div>
   )
 }
+
+function WhoIam() {
+  return (
+    <div id="whoIam">
+
+      <div id="whoIam_header">
+        <img src={MyPhoto} alt="This is me" />
+        <h2>Who I am?</h2>
+      </div>
+      <p> I am a student of computerized information systems at the <a href="http://ut.suagm.edu"
+        target="_blank" rel="noopener noreferrer">University of Turabo</a> and I reside inFlorida,
+        USA. Expecting graduate in 2021, I work for be part of the developer community, learning
+        every day new cool technologies.</p>
+      <p>Do you want to see what I'm doing? Let's take a look below.</p>
+
+      <a id="arrow" href="#achievements"><i class="fas fa-arrow-right"></i></a>
+
+
+    </div>
+  )
+}
+
+function Certifications() {
+  return (
+    <div id="certifications">
+
+    </div>
+  )
+}
+
+function Skills() {
+  return (
+    <div>
+      <Skill />
+    </div>
+  )
+}
+
+function Skill() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+function Projects() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+function Information() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
 
 export default App;
