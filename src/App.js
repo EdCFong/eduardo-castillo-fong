cd419942b4cb47cb3b6f2f7e725d84611e7ac287
Eduardo Castillo Fong <EDUARDCF90@GMAIL.COM>
2021-01-10 19:53:01 -0500

    Update January 10, 2021
    -Working on Skills component
    -Projects componet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import './App.css';
 import MyPhoto from './imagen/Myself.png';
-import iconC from './imagen/CSharp.png'
+import CSharp from './imagen/CSharp.png';
+import XAML from './imagen/XAML.png';
+import SQLite from './imagen/SQLite_Vector_logo2.png';
+import UWP from './imagen/microsoft-store-windows-10.png';
+import WPF from './imagen/wpf.png';
+import HTML from './imagen/kisspng.com-HTML.png';
+import CSS from './imagen/kisspng.com-CSS.png';
+import JS from './imagen/javascript-logo.png';
+import ReactLogo from './imagen/react.png';
+import DateRemember from './imagen/DateRemember-Img.PNG';
+import BestNumber from './imagen/BestNumber.png';
+import QuotesMachine from './imagen/FriendsQuotesMachine.PNG';
+import PomodoroClock from './imagen/Pomodoro Clock.png';
+
+
+
 
 function App() {
   return (
@@ -72,26 +87,91 @@ function Certifications() {
 
 function Skills() {
   return (
-    <div id="skills">
+    <div id="skillsSection">
       <h1>Skills</h1>
-      <Skill />
+      <div id="skills">
+        <Skill name="HTML5" logo={HTML} alt="HTML icon" />
+        <Skill name="CSS" logo={CSS} alt="CSS icon" />
+        <Skill name="Javascript" logo={JS} alt="Javascript icon" />
+        <Skill name="React" logo={ReactLogo} alt="React icon" />
+        <Skill name="C#" logo={CSharp} alt="c sharp icon" />
+        <Skill name="XAML" logo={XAML} alt="XAML icon" />
+        <Skill name="SQLite" logo={SQLite} alt="SQLite icon" />
+        <Skill name="Universal Windows Platform" logo={UWP} alt="UWP icon" />
+        <Skill name="Windows Presentation Foundation" logo={WPF} alt="WPF icon" />
+      </div>
     </div>
   )
 }
 
-function Skill() {
+function Skill(props) {
   return (
-    <div class="skill">
-      <img src={iconC} alt="c sharp icon" />
-      <i>C# programing</i>
+    <div className="skill">
+      <img src={props.logo} alt={props.alt} />
+      <i>{props.name}</i>
     </div>
   )
 }
 
 function Projects() {
   return (
-    <div>
-
+    <div id="projectsSection">
+      <h1>My projects</h1>
+      <div id="projects">
+        <div className="project">
+          <h2>DateRemember app</h2>
+          <div>
+            <a href="https://www.microsoft.com/en-us/p/dateremember/9nxqf7xbr5zm?activetab=pivot:overviewtab">
+              <img src={DateRemember} alt="DateRemember app" />
+            </a>
+            <ul>
+              <li>Is a windows store app, a tool for save and remember birthdays and other important dates</li>
+              <li>Created in Visual Studio using C# and XAML</li>
+              <li>Save the dates in an SQLite database and use the windows notifications for notify the nearest</li>
+            </ul>
+          </div>
+        </div>
+        <div className="project">
+          <h2>BestNumber app</h2>
+          <div>
+            <a href="https://www.microsoft.com/en-us/p/bestnumber/9nblgggxzmqw">
+              <img src={BestNumber} alt="BestNumber app" />
+            </a>
+            <ul>
+              <li>Is a windows store app, a game based on the classic English game of cows and bulls other important dates</li>
+              <li>Created in Visual Studio using C# and XAML</li>
+              <li>Available in the windows store with 5 stars calcification</li>
+            </ul>
+          </div>
+        </div>
+        <div className="project">
+          <h2>Random Quote Machine</h2>
+          <div>
+            <a href="https://codepen.io/edcfong/full/jjXYZV">
+              <img src={QuotesMachine} alt="Random Quote Machine" />
+            </a>
+            <ul>
+              <li>This is a Front End Libraries Course's project from <a href="https://www.freecodecamp.org/">FreeCodeCamp</a></li>
+              <li>Friends TV Show's random quotes generator</li>
+              <li>Created in Visual Studio Code using HTML, CSS, JavaScript and JQuery</li>
+            </ul>
+          </div>
+        </div>
+        <div className="project">
+          <h2>Pomodoro Clock</h2>
+          <div>
+            <a href="https://www.microsoft.com/en-us/p/dateremember/9nxqf7xbr5zm?activetab=pivot:overviewtab">
+              <img src={PomodoroClock} alt="Pomodoro Clock" />
+            </a>
+            <ul>
+              <li>This is a Front End Libraries Course's project from <a href="https://www.freecodecamp.org/">FreeCodeCamp</a></li>
+              <li>Friends TV Show's random quotes generator</li>
+              <li>Created in Visual Studio Code using HTML, CSS, JavaScript and JQuery</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <a href><h2 id="h2-moreProjects">See more projects here</h2></a>
     </div>
   )
 }
@@ -103,6 +183,7 @@ function Information() {
     </div>
   )
 }
+
 
 
 export default App;
