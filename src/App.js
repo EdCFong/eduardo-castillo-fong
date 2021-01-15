af454532e46d880cdb0436beaf7163bbd7be8116
Eduardo Castillo Fong <EDUARDCF90@GMAIL.COM>
2021-01-15 15:51:38 -0500

    Update January 15, 2021
    -Working on Information footer
    -Update names conventions on images

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import './App.css';
-import MyPhoto from './imagen/Myself.png';
-import CSharp from './imagen/CSharp.png';
-import XAML from './imagen/XAML.png';
-import SQLite from './imagen/SQLite_Vector_logo2.png';
-import UWP from './imagen/microsoft-store-windows-10.png';
-import WPF from './imagen/wpf.png';
-import HTML from './imagen/kisspng.com-HTML.png';
-import CSS from './imagen/kisspng.com-CSS.png';
-import JS from './imagen/javascript-logo.png';
-import ReactLogo from './imagen/react.png';
-import DateRemember from './imagen/DateRemember-Img.PNG';
-import BestNumber from './imagen/BestNumber.png';
-import QuotesMachine from './imagen/FriendsQuotesMachine.PNG';
-import PomodoroClock from './imagen/Pomodoro Clock.png';
+
+import MyPhoto_img from './imagen/Myself.png';
+import CSharp_img from './imagen/Skills - CSharp.png';
+import XAML_img from './imagen/Skills - XAML.png';
+import SQLite_img from './imagen/Skills - SQLite.png';
+import UWP_img from './imagen/Skills - Universal Windows Platform Windows Store Logo.png';
+import WPF_img from './imagen/Skills - WPF.png';
+import HTML_img from './imagen/Skills - HTML kisspng.com.png';
+import CSS_img from './imagen/Skills - CSS kisspng.com.png';
+import JS_img from './imagen/Skills - Javascript.png';
+import React_img from './imagen/Skills - React.png';
+import DateRemember_img from './imagen/Projects - DateRemember.png';
+import BestNumber_img from './imagen/Projects - BestNumber.png';
+import QuotesMachine_img from './imagen/Projects - FriendsQuotesMachine.png';
+import PomodoroClock_img from './imagen/Projects - Pomodoro Clock.png';
+import Codepen_img from './imagen/Profiles - Codepen.png';
+import FCC_img from './imagen/Profiles - FreeCodeCamp.png';
+import Github_img from './imagen/Profiles - Github.png';
+import HackerRank_img from './imagen/Profiles - HackerRank.png';
+import Linkedin_img from './imagen/Profiles - Linkedin.png';
+import Sololearn_img from './imagen/Profiles - Sololearn.png';
 
 
 
@@ -57,7 +64,7 @@ function WhoIam() {
     <div id="whoIam">
 
       <div id="whoIam_header">
-        <img src={MyPhoto} alt="This is me" />
+        <img src={MyPhoto_img} alt="This is me" />
         <h2>Who I am?</h2>
       </div>
       <p> I am a student of computerized information systems at the <a href="http://ut.suagm.edu"
@@ -90,15 +97,15 @@ function Skills() {
     <div id="skillsSection">
       <h1>Skills</h1>
       <div id="skills">
-        <Skill name="HTML5" logo={HTML} alt="HTML icon" />
-        <Skill name="CSS" logo={CSS} alt="CSS icon" />
-        <Skill name="Javascript" logo={JS} alt="Javascript icon" />
-        <Skill name="React" logo={ReactLogo} alt="React icon" />
-        <Skill name="C#" logo={CSharp} alt="c sharp icon" />
-        <Skill name="XAML" logo={XAML} alt="XAML icon" />
-        <Skill name="SQLite" logo={SQLite} alt="SQLite icon" />
-        <Skill name="Universal Windows Platform" logo={UWP} alt="UWP icon" />
-        <Skill name="Windows Presentation Foundation" logo={WPF} alt="WPF icon" />
+        <Skill name="HTML5" logo={HTML_img} alt="HTML icon" />
+        <Skill name="CSS" logo={CSS_img} alt="CSS icon" />
+        <Skill name="Javascript" logo={JS_img} alt="Javascript icon" />
+        <Skill name="React" logo={React_img} alt="React icon" />
+        <Skill name="C#" logo={CSharp_img} alt="c sharp icon" />
+        <Skill name="XAML" logo={XAML_img} alt="XAML icon" />
+        <Skill name="SQLite" logo={SQLite_img} alt="SQLite icon" />
+        <Skill name="Universal Windows Platform" logo={UWP_img} alt="UWP icon" />
+        <Skill name="Windows Presentation Foundation" logo={WPF_img} alt="WPF icon" />
       </div>
     </div>
   )
@@ -122,7 +129,7 @@ function Projects() {
           <h2>DateRemember app</h2>
           <div>
             <a href="https://www.microsoft.com/en-us/p/dateremember/9nxqf7xbr5zm?activetab=pivot:overviewtab">
-              <img src={DateRemember} alt="DateRemember app" />
+              <img src={DateRemember_img} alt="DateRemember app" />
             </a>
             <ul>
               <li>Is a windows store app, a tool for save and remember birthdays and other important dates</li>
@@ -135,7 +142,7 @@ function Projects() {
           <h2>BestNumber app</h2>
           <div>
             <a href="https://www.microsoft.com/en-us/p/bestnumber/9nblgggxzmqw">
-              <img src={BestNumber} alt="BestNumber app" />
+              <img src={BestNumber_img} alt="BestNumber app" />
             </a>
             <ul>
               <li>Is a windows store app, a game based on the classic English game of cows and bulls other important dates</li>
@@ -148,7 +155,7 @@ function Projects() {
           <h2>Random Quote Machine</h2>
           <div>
             <a href="https://codepen.io/edcfong/full/jjXYZV">
-              <img src={QuotesMachine} alt="Random Quote Machine" />
+              <img src={QuotesMachine_img} alt="Random Quote Machine" />
             </a>
             <ul>
               <li>This is a Front End Libraries Course's project from <a href="https://www.freecodecamp.org/">FreeCodeCamp</a></li>
@@ -161,7 +168,7 @@ function Projects() {
           <h2>Pomodoro Clock</h2>
           <div>
             <a href="https://www.microsoft.com/en-us/p/dateremember/9nxqf7xbr5zm?activetab=pivot:overviewtab">
-              <img src={PomodoroClock} alt="Pomodoro Clock" />
+              <img src={PomodoroClock_img} alt="Pomodoro Clock" />
             </a>
             <ul>
               <li>This is a Front End Libraries Course's project from <a href="https://www.freecodecamp.org/">FreeCodeCamp</a></li>
@@ -181,64 +188,94 @@ function Information() {
     <footer id="information">
       <h2>My profiles</h2>
       <div id="social_media_box">
-      <div class="social_media">
-                <a href="https://www.linkedin.com/in/eduardo-castillo-fong-0aa094165/?locale=en_US">
-                    <img class="social_media_icon"
-                        src="https://lh3.googleusercontent.com/ZgT35mZzFo3Th14z6AvF6a1Jo9Ll7PLk5hKQRIE4Uxs0wT1mMNwJqmZnGLAuBU9cXhhDeu2qapYMbqwIj569goaVDyCgLf7q60oBajjbHOvc-dQjReno_v48-RTA-D457jRcTUvJXd4=w2400"
-                        alt="Linkedln icon"/>
-                </a>
-                <p class="text-center">Linkedln</p>
-            </div>
-    
-            <div class="social_media">
-                <a id="profile-link" href="https://github.com/EdCFong">
-                    <img id="Github_icon" class="social_media_icon"
-                        src="https://lh3.googleusercontent.com/70yrb8uXmdB28FpjfWFr5uqBdeJLyAwu6v6smhOUR8KYSddkKbPV1f-zaIiObTLWehjvLMHSgQXTuQHjxl-GBpzZBYhkXcO591_Hh6JxbBPULt2uuCxQl25fvQ7zbHAmY57BRaCst5I=w2400"
-                        alt="GitHub icon"/>
-                </a>
-                <p class="text-center">GitHub</p>
-            </div>
-    
-            <div class="social_media">
-                <a href="https://codepen.io/edcfong/">
-                    <img class="social_media_icon"
-                        src="https://lh3.googleusercontent.com/MAJjK7Fm8SBlJdfzloZmv-bqdFt1M1SPEUlvnlD4xjBKymBKvbfSrMOOMH3TSDltH6Jr4qaorSpBupDtNXp5lhlzdHTdikcq6tbeLMp7fkLoohmt0DznGTvndc-yHAy5-2KjRaactrM=w2400"
-                        alt="Codepen icon"/>
-                </a>
-                <p class="text-center">CodePen</p>
-            </div>
-    
-            <div class="social_media">
-                <a href="https://www.freecodecamp.org/fccc7d4b82a-4ffd-4221-b47a-212f1c605b97">
-                    <img id="FreeCodeCamp_icon" class="social_media_icon"
-                        src="https://lh3.googleusercontent.com/thjd6gfnGRv9ciCd2mFwhaabMeQ8jptQfQVaPWqv-sSzTtuuCeguuICXMH7qC-1hFP4orNwJVb8lAo3l0_mme0n6Z3qQrn7pNK44i43Y1xQMzryOJgSL1fUpqWlHkdKI2F62OG9-S3o=w2400"
-                        alt="FreeCodeCamp icon"/>
-                </a>
-                <p class="text-center">FreeCodeCamp</p>
-            </div>
-    
-            <div class="social_media">
-                <a href="https://www.hackerrank.com/eduardcf90">
-                    <img class="social_media_icon"
-                        src="https://lh3.googleusercontent.com/iCR_GJiPovWpluCWSI3X_JTr4laH5gOXZngHHNa2-sCePTIoXHc-3FDVmnzKlc4tQuFO1hQ_9ODLefVc6WpaRsjgApIQ5trsuRrpLKNyr1wp0R5eMXY_vKDg9olhEA8QXsImtPBnxJE=w2400"
-                        alt="HackerRank icon"/>
-                </a>
-                <p class="text-center">HackerRank</p>
-            </div>
-    
-            <div class="social_media">
-                <a href="https://www.sololearn.com/Profile/2399481">
-                    <img class="social_media_icon"
-                        src="https://lh3.googleusercontent.com/LaERs5TZPPkv_Po9LzbrqLSGJzCLBwkUv2MC5bIdaMA4uGdtfDnxAbzdPqra0io0iSqKbd5p_k_miPn0qInk9QItNGh3csTg_YdmBdkAopoRJBXQ-skR34cqb3DrqUz0eyBdKSIxVzU=w2400"
-                        alt="Sololearn icon"/>
-                </a>
-                <p class="text-center">Sololearn</p>
-            </div>
+        <div class="social_media">
+          <a href="https://www.linkedin.com/in/eduardo-castillo-fong-0aa094165/?locale=en_US">
+            <img class="social_media_icon"
+              src={Linkedin_img}
+              alt="Linkedln icon" />
+          </a>
+          <p class="text-center">Linkedln</p>
+        </div>
+
+        <div class="social_media">
+          <a id="profile-link" href="https://github.com/EdCFong">
+            <img id="Github_icon" class="social_media_icon"
+              src={Github_img}
+              alt="GitHub icon" />
+          </a>
+          <p class="text-center">GitHub</p>
+        </div>
+
+        <div class="social_media">
+          <a href="https://codepen.io/edcfong/">
+            <img class="social_media_icon"
+              src={Codepen_img}
+              alt="Codepen icon" />
+          </a>
+          <p class="text-center">CodePen</p>
+        </div>
+
+        <div class="social_media">
+          <a href="https://www.freecodecamp.org/fccc7d4b82a-4ffd-4221-b47a-212f1c605b97">
+            <img id="FreeCodeCamp_icon" class="social_media_icon"
+              src={FCC_img}
+              alt="FreeCodeCamp icon" />
+          </a>
+          <p class="text-center">FreeCodeCamp</p>
+        </div>
+
+        <div class="social_media">
+          <a href="https://www.hackerrank.com/eduardcf90">
+            <img class="social_media_icon"
+              src={HackerRank_img}
+              alt="HackerRank icon" />
+          </a>
+          <p class="text-center">HackerRank</p>
+        </div>
+
+        <div class="social_media">
+          <a href="https://www.sololearn.com/Profile/2399481">
+            <img class="social_media_icon"
+              src={Sololearn_img}
+              alt="Sololearn icon" />
+          </a>
+          <p class="text-center">Sololearn</p>
+        </div>
       </div>
 
-
-
       <h2>Contact me by <inline>eduardcf90@gmail.com</inline></h2>
+      <div id="moreLinks">
+        <div id="personalInformation">
+          <strong>Information</strong>
+          <ul>
+            <li>Story</li>
+            <li>Path</li>
+            <li>All projects</li>
+            <li>All certifications</li>
+            <li>Work History</li>
+          </ul>
+        </div>
+        <div id="resources">
+          <strong>Resources</strong>
+          <ul>
+            <li>tools</li>
+            <li>tutorials</li>
+            <li>Blog</li>
+            <li>Arduino projects</li>
+            <li>Linux</li>
+          </ul>
+        </div>
+        <div id="others">
+          <strong>Others</strong>
+          <ul>
+            <li>Chess</li>
+            <li>sololearn</li>
+            <li>Codepen</li>
+            <li>Quora</li>
+            <li>Games</li>
+          </ul>
+        </div>
+      </div>
     </footer>
   )
 }
